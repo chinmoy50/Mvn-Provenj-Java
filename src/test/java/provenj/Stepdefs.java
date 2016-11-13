af94863fc20ef89dd5dbf1f3dbcfe54564b48a4d
Chuck LeDuc Díaz <celeduc@gmail.com>
2016-11-13 15:58:27 +0100

    finished build-manifest tests

--- src/test/java/provenj/Stepdefs.java
+++ src/test/java/provenj/Stepdefs.java
@@ -5,6 +5,7 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.api.PendingException;
 import org.json.simple.JSONObject;
+import java.util.UUID;
 
 public class Stepdefs {
     // Meaningless templates from cucumber skeleton for Belly
@@ -22,111 +23,106 @@ public class Stepdefs {
 
 
     // Build manifest
+    Manifest manifest = new Manifest();
 
     @Given("^a JPEG file named \"([^\"]*)\"$")
-    public void a_JPEG_file_named(String arg1) throws Throwable {
-	Manifest manifest = new Manifest();
-	manifest.addFile(arg1);
-	JSONObject json = manifest.get();
-	if ( arg1 != (String) json.get("FileName")) {
-            throw new RuntimeException();
-	}
+    public void a_JPEG_file_named(String filename) throws Throwable {
+	manifest.addFile(filename);
     }
 
-    @Given("^the current Bitcoin block number btc_block$")
-    public void the_current_Bitcoin_block_number_btc_block() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Given("^the current Bitcoin block number (\\d+)$")
+    public void the_current_Bitcoin_block_number(int blockNumber) throws Throwable {
+	manifest.setBitcoinBlockNumber(blockNumber);
     }
 
-    @Given("^the current Bitcoin block hash btc_hash$")
-    public void the_current_Bitcoin_block_hash_btc_hash() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Given("^the current Bitcoin block hash \"([^\"]*)\"$")
+    public void the_current_Bitcoin_block_hash(String blockHash) throws Throwable {
+	manifest.setBitcoinBlockHash(blockHash);
     }
 
-    @Given("^the current Ethereum block number eth_block$")
-    public void the_current_Ethereum_block_number_eth_block() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Given("^the current Ethereum block number (\\d+)$")
+    public void the_current_Ethereum_block_number(int blockNumber) throws Throwable {
+	manifest.setEthereumBlockNumber(blockNumber);
     }
 
-    @Given("^the current Ethereum block hash eth_hash$")
-    public void the_current_Ethereum_block_hash_eth_hash() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Given("^the current Ethereum block hash \"([^\"]*)\"$")
+    public void the_current_Ethereum_block_hash(String blockHash) throws Throwable {
+	manifest.setEthereumBlockHash(blockHash);
     }
 
-    @Given("^the IPFS hash from the last submitted file hash_ipfs$")
-    public void the_IPFS_hash_from_the_last_submitted_file_hash_ipfs() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Given("^the IPFS hash from the last submitted file \"([^\"]*)\"$")
+    public void the_IPFS_hash_from_the_last_submitted_file(String ipfsHash) throws Throwable {
+	manifest.setPreviousIPFSHash(ipfsHash);
     }
 
-    @Given("^the other hashes from the last submitted file file_hashes$")
-    public void the_other_hashes_from_the_last_submitted_file_file_hashes() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Given("^the other hashes from the last submitted file \"([^\"]*)\"$")
+    public void the_other_hashes_from_the_last_submitted_file(String fileHashes) throws Throwable {
+	manifest.setPreviousFileHashes(fileHashes);
     }
 
-    @Given("^the GUID for the submission$")
-    public void the_GUID_for_the_submission() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Given("^the GUID for the submission \"([^\"]*)\"$")
+    public void the_GUID_for_the_submission(String guid) throws Throwable {
+	manifest.setGUID(UUID.fromString(guid));
     }
 
     @When("^I ask for a manifest file$")
     public void i_ask_for_a_manifest_file() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        JSONObject json = manifest.get();
     }
 
     @Then("^manifest\\.FileName should equal \"([^\"]*)\"$")
-    public void manifest_FileName_should_equal(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void manifest_FileName_should_equal(String fileName) throws Throwable {
+	if ( fileName != (String) manifest.get().get("FileName")) {
+            throw new RuntimeException();
+	}
     }
 
-    @Then("^manifest\\.BitcoinBlockNumber should match btc_block$")
-    public void manifest_BitcoinBlockNumber_should_match_btc_block() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Then("^manifest\\.BitcoinBlockNumber should be (\\d+)$")
+    public void manifest_BitcoinBlockNumber_should_be(int blockNumber) throws Throwable {
+	if ( blockNumber != (int) manifest.get().get("BitcoinBlockNumber")) {
+            throw new RuntimeException();
+	}
     }
 
-    @Then("^manifest\\.BitcoinLastBlockHash should equal btc_hash$")
-    public void manifest_BitcoinLastBlockHash_should_equal_btc_hash() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-}
-
-    @Then("^manifest\\.EthereumBlockNumber should equal eth_block$")
-    public void manifest_EthereumBlockNumber_should_equal_eth_block() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Then("^manifest\\.BitcoinBlockHash should equal \"([^\"]*)\"$")
+    public void manifest_BitcoinBlockHash_should_equal(String blockHash) throws Throwable {
+	if (!blockHash.equals((String) manifest.get().get("BitcoinBlockHash"))) {
+            throw new RuntimeException();
+	}
     }
 
-    @Then("^manifest\\.EthereumLastBlockHash should equal eth_hash$")
-    public void manifest_EthereumLastBlockHash_should_equal_eth_hash() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Then("^manifest\\.EthereumBlockNumber should equal (\\d+)$")
+    public void manifest_EthereumBlockNumber_should_equal(int blockNumber) throws Throwable {
+	if (blockNumber != (int) manifest.get().get("EthereumBlockNumber")) {
+            throw new RuntimeException();
+	}
     }
 
-    @Then("^manifest\\.PreviousIFPSHash should equal hash_ipfs$")
-    public void manifest_PreviousIFPSHash_should_equal_hash_ipfs() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Then("^manifest\\.EthereumBlockHash should equal \"([^\"]*)\"$")
+    public void manifest_EthereumBlockHash_should_equal(String blockHash) throws Throwable {
+	if (!blockHash.equals((String) manifest.get().get("EthereumBlockHash"))) {
+            throw new RuntimeException();
+	}
     }
 
-    @Then("^manifest\\.FileHashes should equal file_hashes$")
-    public void manifest_FileHashes_should_equal_file_hashes() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Then("^manifest\\.PreviousIFPSHash should equal \"([^\"]*)\"$")
+    public void manifest_PreviousIFPSHash_should_equal(String ifpsHash) throws Throwable {
+	if (!ifpsHash.equals((String) manifest.get().get("PreviousIPFSHash"))) {
+            throw new RuntimeException();
+	}
     }
 
-    @Then("^manifest\\.GUID should equal GUID$")
-    public void manifest_GUID_should_equal_GUID() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Then("^manifest\\.PreviousFileHashes should equal \"([^\"]*)\"$")
+    public void manifest_PreviousFileHashes_should_equal(String fileHashes) throws Throwable {
+	if (!fileHashes.equals((String) manifest.get().get("PreviousFileHashes"))) {
+            throw new RuntimeException();
+	}
     }
 
+    @Then("^manifest\\.GUID should equal \"([^\"]*)\"$")
+    public void manifest_GUID_should_equal(String guid) throws Throwable {
+	if (!guid.equals((String) manifest.get().get("GUID"))) {
+            throw new RuntimeException();
+	}
+    }
 }
--- src/test/resources/provenj/build-manifest.feature
+++ src/test/resources/provenj/build-manifest.feature
@@ -3,19 +3,20 @@ Feature: Build a manifest of the files supplied to Proven
 
   Scenario: A picture needs to be listed in the manifest
     Given a JPEG file named "IMG_00001.jpeg"
-    And the current Bitcoin block number btc_block
-    And the current Bitcoin block hash btc_hash
-    And the current Ethereum block number eth_block
-    And the current Ethereum block hash eth_hash
-    And the IPFS hash from the last submitted file hash_ipfs
-    And the other hashes from the last submitted file file_hashes
-    And the GUID for the submission
+    And the current Bitcoin block number 438712
+    And the current Bitcoin block hash "00000000000000000341b8be019c7c1bff721b2d412285ab796ef5ab2ae36213"
+    And the current Ethereum block number 2619567
+    And the current Ethereum block hash "0xcca5f32676df55171ffabcb32b69eaec0593ce7ae1912228656bd8e213475ecb"
+    And the IPFS hash from the last submitted file "QmP1KyrSsD4KGPFRsVxV66cZ95LqhLWGbwCakzRsoKjrTu"
+    # TODO: multiple hashes. for now, SHA256
+    And the other hashes from the last submitted file "dff0c94255cd1f68a824e81005b00f617afecd74c6cccecfbae0d2b7875fabf3"
+    And the GUID for the submission "0b89ff5d-c1d8-4dce-949a-a7e29215b09d"
     When I ask for a manifest file
     Then manifest.FileName should equal "IMG_00001.jpeg"
-    And manifest.BitcoinBlockNumber should match btc_block
-    And manifest.BitcoinLastBlockHash should equal btc_hash
-    And manifest.EthereumBlockNumber should equal eth_block
-    And manifest.EthereumLastBlockHash should equal eth_hash
-    And manifest.PreviousIFPSHash should equal hash_ipfs
-    And manifest.FileHashes should equal file_hashes
-    And manifest.GUID should equal GUID
+    And manifest.BitcoinBlockNumber should be 438712
+    And manifest.BitcoinBlockHash should equal "00000000000000000341b8be019c7c1bff721b2d412285ab796ef5ab2ae36213"
+    And manifest.EthereumBlockNumber should equal 2619567
+    And manifest.EthereumBlockHash should equal "0xcca5f32676df55171ffabcb32b69eaec0593ce7ae1912228656bd8e213475ecb"
+    And manifest.PreviousIFPSHash should equal "QmP1KyrSsD4KGPFRsVxV66cZ95LqhLWGbwCakzRsoKjrTu"
+    And manifest.PreviousFileHashes should equal "dff0c94255cd1f68a824e81005b00f617afecd74c6cccecfbae0d2b7875fabf3"
+    And manifest.GUID should equal "0b89ff5d-c1d8-4dce-949a-a7e29215b09d"
