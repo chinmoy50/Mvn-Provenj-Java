15846b38322c88091e55766393b77170a48b593b
Chuck LeDuc Díaz <celeduc@gmail.com>
2016-12-04 13:58:38 +0100

    make constant class into interface and reorder

--- src/main/java/provenj/Manifest.java
+++ src/main/java/provenj/Manifest.java
@@ -90,13 +90,13 @@ public class Manifest {
     public JSONObject get(){
        JSONObject result = new JSONObject();
        result.put(ProvenLib.PROVEN_FILE_NAME,             getFileName());
+       result.put(ProvenLib.PROVEN_FILE_HASHES,           getFileHashes());
        result.put(ProvenLib.PROVEN_BITCOIN_BLOCK_NUMBER,  getBitcoinBlockNumber());
        result.put(ProvenLib.PROVEN_BITCOIN_BLOCK_HASH,    getBitcoinBlockHash());
        result.put(ProvenLib.PROVEN_ETHEREUM_BLOCK_NUMBER, getEthereumBlockNumber());
        result.put(ProvenLib.PROVEN_ETHEREUM_BLOCK_HASH,   getEthereumBlockHash());
        result.put(ProvenLib.PROVEN_PREVIOUS_IPFS_HASH,    getPreviousIPFSHash());
        result.put(ProvenLib.PROVEN_PREVIOUS_FILE_HASHES,  getPreviousFileHashes());
-       result.put(ProvenLib.PROVEN_FILE_HASHES,           getFileHashes());
        result.put(ProvenLib.PROVEN_GUID,                  getGUID().toString());
        return result;
     }
--- src/main/java/provenj/ProvenLib.java
+++ src/main/java/provenj/ProvenLib.java
@@ -1,15 +1,16 @@
 package provenj;
 
-public class ProvenLib {
+// Constants needed across Proven. This is in an interface because it is never instantiated.
+public interface ProvenLib {
     public static String PROVEN_NAMESPACE             = "http://ns.facted.net/proven/1.0/facted/";
     public static String PROVEN_PREFIX                = "Proven";
     public static String PROVEN_FILE_NAME             = "FileName";
+    public static String PROVEN_FILE_HASHES           = "FileHashes";
     public static String PROVEN_BITCOIN_BLOCK_NUMBER  = "BitcoinBlockNumber";
     public static String PROVEN_BITCOIN_BLOCK_HASH    = "BitcoinBlockHash";
     public static String PROVEN_ETHEREUM_BLOCK_NUMBER = "EthereumBlockNumber";
     public static String PROVEN_ETHEREUM_BLOCK_HASH   = "EthereumBlockHash";
     public static String PROVEN_PREVIOUS_IPFS_HASH    = "PreviousIPFSHash";
     public static String PROVEN_PREVIOUS_FILE_HASHES  = "PreviousFileHashes";
-    public static String PROVEN_FILE_HASHES           = "FileHashes";
     public static String PROVEN_GUID                  = "GUID";
 }
