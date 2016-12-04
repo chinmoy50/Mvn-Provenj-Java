13b3fbbe18cc12b021dc634158196dfede182ccf
Chuck LeDuc Díaz <celeduc@gmail.com>
2016-12-04 14:01:44 +0100

    remove unnecessary try/catch

--- src/main/java/provenj/ImageTags.java
+++ src/main/java/provenj/ImageTags.java
@@ -52,21 +52,18 @@ public class ImageTags {
     }
 
     public FileOutputStream getFile() throws IOException, XMPException {
-        try {
-            XMPMeta meta = XmpUtil.createXMPMeta();
+        XMPMeta meta = XmpUtil.createXMPMeta();
 
-            meta.setProperty(XmpUtil.PROVEN_NAMESPACE, ProvenLib.PROVEN_BITCOIN_BLOCK_NUMBER, m_bitcoinBlockNumber);
-            meta.setProperty(XmpUtil.PROVEN_NAMESPACE, ProvenLib.PROVEN_BITCOIN_BLOCK_HASH, m_bitcoinBlockHash);
-            meta.setProperty(XmpUtil.PROVEN_NAMESPACE, ProvenLib.PROVEN_ETHEREUM_BLOCK_NUMBER, m_ethereumBlockNumber);
-            meta.setProperty(XmpUtil.PROVEN_NAMESPACE, ProvenLib.PROVEN_ETHEREUM_BLOCK_HASH, m_ethereumBlockHash);
-            meta.setProperty(XmpUtil.PROVEN_NAMESPACE, ProvenLib.PROVEN_PREVIOUS_IPFS_HASH, m_previousIPFSHash);
-            meta.setProperty(XmpUtil.PROVEN_NAMESPACE, ProvenLib.PROVEN_PREVIOUS_FILE_HASHES, m_previousFileHashes);
-            meta.setProperty(XmpUtil.PROVEN_NAMESPACE, ProvenLib.PROVEN_GUID, m_guid.toString());
+        meta.setProperty(XmpUtil.PROVEN_NAMESPACE, ProvenLib.PROVEN_BITCOIN_BLOCK_NUMBER, m_bitcoinBlockNumber);
+        meta.setProperty(XmpUtil.PROVEN_NAMESPACE, ProvenLib.PROVEN_BITCOIN_BLOCK_HASH, m_bitcoinBlockHash);
+        meta.setProperty(XmpUtil.PROVEN_NAMESPACE, ProvenLib.PROVEN_ETHEREUM_BLOCK_NUMBER, m_ethereumBlockNumber);
+        meta.setProperty(XmpUtil.PROVEN_NAMESPACE, ProvenLib.PROVEN_ETHEREUM_BLOCK_HASH, m_ethereumBlockHash);
+        meta.setProperty(XmpUtil.PROVEN_NAMESPACE, ProvenLib.PROVEN_PREVIOUS_IPFS_HASH, m_previousIPFSHash);
+        meta.setProperty(XmpUtil.PROVEN_NAMESPACE, ProvenLib.PROVEN_PREVIOUS_FILE_HASHES, m_previousFileHashes);
+        meta.setProperty(XmpUtil.PROVEN_NAMESPACE, ProvenLib.PROVEN_GUID, m_guid.toString());
 
-            XmpUtil.writeXMPMeta(m_inputFile, m_outputFile, meta);
-        } catch (Exception e) {
-            throw e;
-        }
+        XmpUtil.writeXMPMeta(m_inputFile, m_outputFile, meta);
+
         return m_outputFile;
     }
 }
