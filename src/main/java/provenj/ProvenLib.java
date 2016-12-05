fdd3ed6232e4edf6cea64c8c3fb21344c1479309
Chuck LeDuc Díaz <celeduc@gmail.com>
2016-12-05 20:27:56 +0100

    better comments in ProvenLib

--- src/main/java/provenj/ProvenLib.java
+++ src/main/java/provenj/ProvenLib.java
@@ -2,11 +2,13 @@ package provenj;
 
 // Constants needed across Proven. This is in an interface because it is never instantiated.
 public interface ProvenLib {
-    String PROVEN_NAMESPACE             = "http://ns.facted.net/proven/1.0/facted/";
-    String PROVEN_PREFIX                = "Proven";
+    // These define the structure of the enclosure posted to IPFS with each submission.
     String PROVEN_INDEX                 = "index.html";
     String PROVEN_MANIFEST              = "manifest.json";
     String PROVEN_CONTENT_DIRECTORY     = "content";
+
+    // These are the names of XMP tags that appear within submitted files
+    // and the same tags in the manifest and index.
     String PROVEN_FILE_NAME             = "FileName";
     String PROVEN_FILE_HASHES           = "FileHashes";
     String PROVEN_BITCOIN_BLOCK_NUMBER  = "BitcoinBlockNumber";
@@ -16,4 +18,8 @@ public interface ProvenLib {
     String PROVEN_PREVIOUS_IPFS_HASH    = "PreviousIPFSHash";
     String PROVEN_PREVIOUS_FILE_HASHES  = "PreviousFileHashes";
     String PROVEN_GUID                  = "GUID";
+
+    // These define the XMP namespace and tag prefix used within files
+    String PROVEN_NAMESPACE             = "http://ns.facted.net/proven/1.0/facted/";
+    String PROVEN_PREFIX                = "Proven";
 }
