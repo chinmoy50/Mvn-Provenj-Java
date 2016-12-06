92384715e16cbc25fcb19eb23f21adf64ea017c2
Chuck LeDuc Díaz <celeduc@gmail.com>
2016-12-06 12:53:29 +0100

    test against the metadata returned

--- src/test/java/provenj/Stepdefs.java
+++ src/test/java/provenj/Stepdefs.java
@@ -144,35 +144,41 @@ public class Stepdefs {
     public void the_Bitcoin_block_hash_everywhere_is(String blockHash) throws Throwable {
         assertEquals(blockHash, finalJson.get(ProvenLib.PROVEN_BITCOIN_BLOCK_HASH));
         assertEquals(blockHash, getFinalImageTag(ProvenLib.PROVEN_BITCOIN_BLOCK_HASH));
+        assertEquals(blockHash, metadata.getBitcoinBlockHash());
     }
 
     @Then("^the Bitcoin block number everywhere is (\\d+)$")
     public void the_Bitcoin_block_number_everywhere_is(int blockNumber) throws Throwable {
         assertEquals(Integer.toString(blockNumber), finalJson.get(ProvenLib.PROVEN_BITCOIN_BLOCK_NUMBER).toString());
         assertEquals(Integer.toString(blockNumber), getFinalImageTag(ProvenLib.PROVEN_BITCOIN_BLOCK_NUMBER));
+        assertEquals(blockNumber, metadata.getBitcoinBlockNumber());
     }
 
     @Then("^the Ethereum block hash everywhere is \"([^\"]*)\"$")
     public void the_Ethereum_block_hash_everywhere_is(String blockHash) throws Throwable {
         assertEquals(blockHash, finalJson.get(ProvenLib.PROVEN_ETHEREUM_BLOCK_HASH));
         assertEquals(blockHash, getFinalImageTag(ProvenLib.PROVEN_ETHEREUM_BLOCK_HASH));
+        assertEquals(blockHash, metadata.getEthereumBlockHash());
     }
 
     @Then("^the Ethereum block number everywhere is (\\d+)$")
     public void the_Ethereum_block_number_everywhere_is(int blockNumber) throws Throwable {
         assertEquals(Integer.toString(blockNumber), finalJson.get(ProvenLib.PROVEN_ETHEREUM_BLOCK_NUMBER).toString());
         assertEquals(Integer.toString(blockNumber), getFinalImageTag(ProvenLib.PROVEN_ETHEREUM_BLOCK_NUMBER));
+        assertEquals(blockNumber, metadata.getEthereumBlockNumber());
     }
 
     @Then("^the last IPFS file hash everywhere is \"([^\"]*)\"$")
     public void the_last_IPFS_file_hash_everywhere_is(String ipfsHash) throws Throwable {
         assertEquals(ipfsHash, finalJson.get(ProvenLib.PROVEN_PREVIOUS_IPFS_HASH));
         assertEquals(ipfsHash, getFinalImageTag(ProvenLib.PROVEN_PREVIOUS_IPFS_HASH));
+        assertEquals(ipfsHash, metadata.getPreviousIPFSHash());
     }
 
     @Then("^the last file hashes everywhere is \"([^\"]*)\"$")
     public void the_last_file_hashes_everywhere_is(String fileHashes) throws Throwable {
         assertEquals(fileHashes, finalJson.get(ProvenLib.PROVEN_PREVIOUS_FILE_HASHES));
         assertEquals(fileHashes, getFinalImageTag(ProvenLib.PROVEN_PREVIOUS_FILE_HASHES));
+        assertEquals(fileHashes, metadata.getPreviousFileHashes());
     }
 }
