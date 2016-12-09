3615b8bf9a474523cce6ac3d73e02dbf43e1b781
Chuck LeDuc Díaz <celeduc@gmail.com>
2016-12-09 15:56:13 +0100

    consolidate exceptions

--- src/main/java/provenj/CmdLine.java
+++ src/main/java/provenj/CmdLine.java
@@ -9,10 +9,7 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Paths;
-import java.security.NoSuchAlgorithmException;
 
 // Command-line interface for Provenj
 public class CmdLine {
@@ -41,21 +38,9 @@ public class CmdLine {
             metadata = enclosure.fillEnclosure(Paths.get(args[0]),metadata);
             System.out.println(enclosure.getPath());
         }
-        catch( ParseException exp ) {
+        catch( Exception e ) {
             // oops, something went wrong
-            System.err.println( "Parsing failed. Reason: " + exp.getMessage() );
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (XMPException e) {
-            e.printStackTrace();
+            System.err.println("Parsing failed. Reason: " + e.getMessage());
         }
     }
 }
