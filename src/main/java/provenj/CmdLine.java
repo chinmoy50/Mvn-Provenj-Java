9744bd2241ab8a9e6792e83e622a1b962c74be84
Chuck LeDuc Díaz <celeduc@gmail.com>
2016-12-09 14:25:42 +0100

    WIP command line

--- src/main/java/provenj/CmdLine.java
+++ src/main/java/provenj/CmdLine.java
@@ -1,8 +1,32 @@
 package provenj;
 
-/**
- * Created by cleduc on 09/12/16.
- */
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
+// Command-line interface for Provenj
 public class CmdLine {
+    public static void main( String[] args ) {
+        // create the parser
+        CommandLineParser parser = new DefaultParser();
+        Options options = new Options();
+        options.addOption(Option.builder("D")
+                .hasArgs()
+                .valueSeparator('=')
+                .build());
+
+        try {
+            // parse the command line arguments
+            CommandLine line = parser.parse( options, args );
+
+            System.out.println(line.toString());
+        }
+        catch( ParseException exp ) {
+            // oops, something went wrong
+            System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
+        }
+    }
 }
