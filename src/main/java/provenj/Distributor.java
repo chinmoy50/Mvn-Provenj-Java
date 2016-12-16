e6de4895f8dd7ce098e44ed1d3e37b997609c5d8
Chuck LeDuc Díaz <celeduc@gmail.com>
2016-12-16 16:01:38 +0100

    remove default constructor

--- src/main/java/provenj/Distributor.java
+++ src/main/java/provenj/Distributor.java
@@ -9,8 +9,6 @@ import io.ipfs.api.NamedStreamable;
 
 // Distributes a file over IPFS
 public class Distributor {
-    public Distributor() {}
-
     public String publishIPFS(Path path) throws IOException {
         IPFS ipfs = new IPFS("/ip4/127.0.0.1/tcp/5001");
         MerkleNode addResult = ipfs.add(new NamedStreamable.FileWrapper(path.toFile()));
