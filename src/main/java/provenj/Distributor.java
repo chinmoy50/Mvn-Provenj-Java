bd3e8667a9dc3ec687bd8412a73fd815294d89e2
Chuck LeDuc Díaz <celeduc@gmail.com>
2016-12-28 08:44:28 +0100

    kotlin api WIP

--- src/main/java/provenj/Distributor.java
+++ src/main/java/provenj/Distributor.java
@@ -3,15 +3,11 @@ package provenj;
 import java.io.IOException;
 import java.nio.file.Path;
 
-import io.ipfs.api.IPFS;
-import io.ipfs.api.MerkleNode;
-import io.ipfs.api.NamedStreamable;
+import io.ipfs.kotlin.IPFS;
 
 // Distributes a file over IPFS
 public class Distributor {
     public String publishIPFS(Path path) throws IOException {
-        IPFS ipfs = new IPFS("/ip4/127.0.0.1/tcp/5001");
-        MerkleNode addResult = ipfs.add(new NamedStreamable.FileWrapper(path.toFile()));
-        return (addResult.hash.toString());
+        return new IPFS().getAdd().file(path.toFile()).getHash();
     }
 }
