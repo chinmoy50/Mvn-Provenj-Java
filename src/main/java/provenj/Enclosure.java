7b72df7f85788f3ebb122da87c428586f28cd0b1
Chuck LeDuc Díaz <celeduc@gmail.com>
2017-01-29 14:51:15 +0100

    createParentDirs breaks test

--- src/main/java/provenj/Enclosure.java
+++ src/main/java/provenj/Enclosure.java
@@ -23,8 +23,8 @@ public class Enclosure {
     protected Path m_path;
 
     protected void init() throws IOException {
-        m_path = Files.createTempDirectory(ProvenLib.PROVEN_PREFIX);
-        Files.createDirectory(Paths.get(getPath(ProvenLib.PROVEN_CONTENT_DIRECTORY)));
+        m_path = Files.createTempDir().toPath();
+        Files.createParentDirs(Paths.get(getPath(ProvenLib.PROVEN_CONTENT_DIRECTORY)).toFile());
     }
 
     public Enclosure() throws IOException {
